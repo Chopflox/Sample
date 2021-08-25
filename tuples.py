7f1ff189c9b54b8400dd3554f852003d7348286b
Chopflox <elias_burkan95@hotmail.com>
2021-08-25 09:50:59 +0200

    oppdatert

--- tuples.py
+++ tuples.py
@@ -1,18 +1,7 @@
-i = 1
-b = 2
+# Write your solution here
 
-cities = ['oslo', 'frankrike','København']
-citiesTuple1 = ('oslo', '0010','frankrike', '5500','København', '1234')
-citiesTuple2 = ['frankrike', '5500']
-citiesTuple3 = ['København', '1234']
+cities = [('oslo', '0010'), ('frankrike', '5500'),('København','1234')]
 
-print(cities)
-while i < b:
-    print(citiesTuple1)
-    i = 2
-    
-(city, zipc) = citiesTuple2
 
-print(city)
-
-print(zipc)    
+for (city, zipc) in cities:
+    print(city, zipc)
