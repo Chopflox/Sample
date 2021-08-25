46e1cbb7622e25bf267eee19a265a1a16974a20e
Chopflox <elias_burkan95@hotmail.com>
2021-08-25 09:39:02 +0200

    øver med på tuples

---
+++ tuples.py
@@ -0,0 +1,18 @@
+i = 1
+b = 2
+
+cities = ['oslo', 'frankrike','København']
+citiesTuple1 = ('oslo', '0010','frankrike', '5500','København', '1234')
+citiesTuple2 = ['frankrike', '5500']
+citiesTuple3 = ['København', '1234']
+
+print(cities)
+while i < b:
+    print(citiesTuple1)
+    i = 2
+    
+(city, zipc) = citiesTuple2
+
+print(city)
+
+print(zipc)    
